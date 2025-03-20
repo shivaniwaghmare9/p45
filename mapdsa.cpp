e8a13e07877fb0e4d3f6ea7d15bc202d48d9195e
shivani <shivaniw69@gmail.com>
2025-03-20 23:22:04 +0530

    map

--- mapdsa.cpp
+++ mapdsa.cpp
@@ -19,3 +19,27 @@ int main()
     }
     
 }
+// Online C++ compiler to run C++ program online
+//=====================================MAP================================================================================================================
+#include <iostream>
+using namespace std;
+#include <map>
+int main()
+{
+    map<int ,string>mp;
+    mp.insert({102,"pihu"});
+    mp.insert({10,"hny"});
+    mp.insert({101,"neha"});
+   mp.insert({11,"gyani"}); 
+    for(auto p:mp)
+    {
+        cout<<p.first<<","<<p.second<<"\n";
+    }
+    cout<<"Size="<<mp.size()<<"\n";
+    for(auto p=mp.begin(); p!=mp.end();p++)
+    {
+        cout<<p->first<<","<<p->second<<"\n";
+    }
+    
+    
+}
