3e101ed88ac8fc098774020f133db804401b49a7
shivani <shivaniw69@gmail.com>
2025-03-21 18:11:21 +0530

    new

--- mapdsa.cpp
+++ mapdsa.cpp
@@ -152,4 +152,109 @@ int main() {
    for(auto p:mp){
        cout<<p.first<<":"<<p.second<<"\n";
    }
+   
+}
+// Online C++ compiler to run C++ program online
+//====================================MAP(INPUT BY USER )=======================================================================================================
+#include <iostream>
+using namespace std;
+#include <map>
+int main() {
+    map<int,string>mp;
+    int roll,n;
+    string name;
+    cout<<"\nenter how many records\n";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cout<<"\nenter roll no:";
+        cin>>roll;
+        cout<<"\nenter name:";
+        cin>>name;
+        mp.insert({roll,name});
+        //mp.insert(make_pair(roll,name));
+    }
+   cout<<"\nrecodds are\n";
+   for(auto p:mp){
+       cout<<p.first<<":"<<p.second<<"\n";
+   }
+   //find() function or searching function
+   cout<<"\n enter roll no searching\n";
+   cin>>roll;
+   auto k=mp.find(roll);     //search or find
+   if(k!=mp.end()){
+       cout<<k->first<<":"<<k->second<<"\n";
+       
+   }
+   else{
+       cout<<"not found";
+   }
+   //delete  
+   cout<<"\n enter roll no delete\n";
+   cin>>roll;
+   auto j=mp.find(roll);     //delete or erase
+   if(j!=mp.end()){
+       mp.erase(roll);
+       
+   }
+   else{
+       cout<<"not found";
+   }
+   cout<<"\n";
+   for(auto p:mp){
+       cout<<p.first<<":"<<p.second<<"\n";
+   }
+   
+   
+}
+// Online C++ compiler to run C++ program online
+//====================================UNORDERDED_MAP(INPUT BY USER )(unsordet data aata h)=======================================================================================================
+#include <iostream>
+using namespace std;
+#include <unordered_map>
+int main() {
+    unordered_map<int,string>up;
+    int roll,n;
+    string name;
+    cout<<"\nenter how many records\n";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cout<<"\nenter roll no:";
+        cin>>roll;
+        cout<<"\nenter name:";
+        cin>>name;
+        up.insert({roll,name});
+        //mp.insert(make_pair(roll,name));
+    }
+   cout<<"\nrecodds are\n";
+   for(auto p:up){
+       cout<<p.first<<":"<<p.second<<"\n";
+   }
+   //find() function or searching function
+   cout<<"\n enter roll no searching\n";
+   cin>>roll;
+   auto k=up.find(roll);     //search or find
+   if(k!=up.end()){
+       cout<<k->first<<":"<<k->second<<"\n";
+       
+   }
+   else{
+       cout<<"not found";
+   }
+   //delete  
+   cout<<"\n enter roll no delete\n";
+   cin>>roll;
+   auto j=up.find(roll);     //delete or erase
+   if(j!=up.end()){
+       up.erase(roll);
+       
+   }
+   else{
+       cout<<"not found";
+   }
+   cout<<"\n";
+   for(auto p:up){
+       cout<<p.first<<":"<<p.second<<"\n";
+   }
+   
+   
 }
