d53e9d1d4209347754824169e87eebfb0cf7fbce
shivani <shivaniw69@gmail.com>
2026-01-03 22:59:00 +0530

    dsa

--- paranthesessdsa.cpp
+++ paranthesessdsa.cpp
@@ -1,5 +1,112 @@
 
 
+// // Online C++ compiler to run C++ program online
+// //=========================================paranthesess=================================================================================================
+// #include <iostream>
+// using namespace std;
+// int valid(string s)
+// {
+//     if(s.size()<=4)
+//     {
+//         return 0;
+//     }
+//     else
+//     {
+//         return 1;
+//     }
+// }
+// int main() 
+// {
+//     string s;
+//     cout<<"enter name\n";
+//     cin>>s;
+//     bool j=valid(s);
+//     if(j){
+//         cout<<"valid";
+//     }
+//     else{
+//         cout<<"invalid";
+//     }
+// }
+// // Online C++ compiler to run C++ program online
+// //====================================================PARANTHESESS=================================================================================================================================================================================================
+// #include <iostream>
+// using namespace std;
+// #include<stack>
+// bool pvalid(string s)
+// {
+//     stack<char>stk;
+//     bool r=true;
+//     for(int i=0; i<s.size(); i++)
+//     {
+//         if(s[i]=='{' || s[i]=='(' || s[i]=='[')
+//         {
+//             stk.push(s[i]);
+//         }
+//         else if(s[i]=='}')
+//         {
+//             if(!stk.empty()&& stk.top()=='{')
+//             {
+//                 stk.pop();
+//             }
+//             else
+//             {
+//                 r=false;
+//                 break;
+//             }
+//         }
+            
+//             else if(s[i]==')')
+//         {
+//             if(!stk.empty()&& stk.top()=='(')
+//             {
+//                 stk.pop();
+//             }
+//             else
+//             {
+//                 r=false;
+//                 break;
+//             }
+//         }
+//             else if(s[i]==']')
+//         {
+//             if(!stk.empty()&& stk.top()=='[')
+//             {
+//                 stk.pop();
+//             }
+//             else
+//             {
+//                 r=false;
+//                 break;
+//             }
+//         }
+//     }
+//         if(!stk.empty())
+//         {
+//             return false;
+//         }
+//         else
+//         {
+//             return r;
+//         }
+// }
+
+
+// int main() 
+// {
+//    string s="{[()]}";
+//    if(pvalid(s))
+//    {
+//        cout<<"valid";
+//    }
+//    else 
+//    {
+//        cout<<"invalid";
+//    }
+   
+// }
+
+
 // Online C++ compiler to run C++ program online
 //=========================================paranthesess=================================================================================================
 #include <iostream>
